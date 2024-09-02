e0b9dd1fcf9a79b7d53b8a98147ea89b16d902d7
Bacem Jaza <bacemjaza7@gmail.com>
2024-09-02 15:35:59 +0100

    update tab

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,7 +31,8 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en-GB'>
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
+          <title>Portfolio - Bacem Jaza</title>
+          <link rel='icon' href="/images/icon.png"/>
         </Head>
         <body>
           <Main />
